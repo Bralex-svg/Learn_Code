432196c9743380c26b7a4cc902fd42b9f5f421cb
Bralex-svg <alexadam269@gmail.com>
2023-03-03 11:26:19 -0800

    how to component added

--- pages/components/HowTo.tsx
+++ pages/components/HowTo.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+const Icon1 = "/icon1.png";
+const Icon2 = "/icon2.png";
+const Icon3 = "/icon3.png";
 
 const HowTo = () => {
   return (
     <>
-      <div>How it works</div>
+      <div className="ml-[10%]">
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          <div className="border w-[60%]">
+            <img className="w-[25%] ml-[20%]" src={Icon1} alt="" />
+            <h2 className="font-bold ml-[20%]">Sign Up</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+              at.
+            </p>
+          </div>
+          <div className="bg-[#00C2ED] border w-[60%] h-[100%]">
+            <img className="w-[25%] ml-[20%]" src={Icon2} alt="" />
+            <h2
+              className="font-bold ml-[20%] text-white
+            "
+            >
+              Enroll In A Course
+            </h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+              at.
+            </p>
+          </div>
+          <div className="border w-[60%]">
+            <img className="w-[25%] ml-[20%]" src={Icon3} alt="" />
+            <h2 className="font-bold ml-[20%]">Start Learning</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+              at.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
