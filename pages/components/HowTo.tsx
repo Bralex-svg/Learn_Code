32c0f45581a4ff8a0a09da7222f89efecc275df2
Bralex-svg <alexadam269@gmail.com>
2023-03-06 09:58:42 -0800

    added more styling to this comp

--- pages/components/HowTo.tsx
+++ pages/components/HowTo.tsx
@@ -6,33 +6,52 @@ const Icon3 = "/icon3.png";
 const HowTo = () => {
   return (
     <>
-      <div className="ml-[10%]">
+      <div className="ml-[20%] w-[70%]">
+        <h1 className="font-bold ml-[10%] md:ml-[30%] relative md:-translate-y-[150%] text-3xl">
+          HOW IT WORKS?
+        </h1>
         <div className="grid grid-cols-1 md:grid-cols-3">
-          <div className="border w-[60%]">
-            <img className="w-[25%] ml-[20%]" src={Icon1} alt="" />
-            <h2 className="font-bold ml-[20%]">Sign Up</h2>
-            <p>
+          <div className="border w-[80%]">
+            <img
+              className="w-[25%] relative top-[5%] ml-[30%]"
+              src={Icon1}
+              alt=""
+            />
+            <h2 className="font-bold relative md:top-[8%] text-2xl ml-[25%]">
+              Sign Up
+            </h2>
+            <p className="md:mt-[5%] ml-[7%]">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
               at.
             </p>
           </div>
-          <div className="bg-[#00C2ED] border w-[60%] h-[100%]">
-            <img className="w-[25%] ml-[20%]" src={Icon2} alt="" />
+          <div className="bg-[#00C2ED] border w-[80%] h-[100%]">
+            <img
+              className="w-[25%] relative md:top-[5%] ml-[30%]"
+              src={Icon2}
+              alt=""
+            />
             <h2
-              className="font-bold ml-[20%] text-white
+              className="font-bold ml-[10%] text-2xl relative md:top-[10%] text-white
             "
             >
               Enroll In A Course
             </h2>
-            <p>
+            <p className="md:mt-[8%] ml-[7%]">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
               at.
             </p>
           </div>
-          <div className="border w-[60%]">
-            <img className="w-[25%] ml-[20%]" src={Icon3} alt="" />
-            <h2 className="font-bold ml-[20%]">Start Learning</h2>
-            <p>
+          <div className="border w-[80%]">
+            <img
+              className="w-[25%] relative md:top-[5%]  ml-[30%]"
+              src={Icon3}
+              alt=""
+            />
+            <h2 className="font-bold text-2xl md:top-[10%] relative  ml-[20%]">
+              Start Learning
+            </h2>
+            <p className="md:mt-[8%] ml-[7%]">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
               at.
             </p>
