605ce877d7d2b2dd669b762a4f0673b215108c01
Bralex-svg <alexadam269@gmail.com>
2023-03-10 14:19:28 -0800

    updating

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -15,11 +15,11 @@ const Courses = () => {
     <>
       <div>
         <h1>Browse Our Courses</h1>
-        <div>
-          <Grid container spacing={3}>
+        <div className="translate-y-[30%] ]">
+          <Grid className="w-[60%]" container spacing={3}>
             {data.courses.map((course) => (
               <Grid item xs={12} sm={6} md={4} key={course.name}>
-                <Card>
+                <Card className="w-[100%]">
                   <CardActionArea>
                     <CardMedia
                       component="img"
