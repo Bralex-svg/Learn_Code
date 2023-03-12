0deaf92b328a9842f5f77102a99e0e85a9686fa6
Bralex-svg <alexadam269@gmail.com>
2023-03-12 10:09:47 -0700

    updating

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -21,20 +21,20 @@ const Courses = () => {
           Browse Our Courses
         </h1>
         <div className="translate-y-[30%]">
-          <Grid className="w-[60%]" container spacing={3}>
+          <Grid className="w-[80%] ml-[10%]" container spacing={3}>
             {data.courses.map((course) => (
               <Grid item xs={12} sm={6} md={4} key={course.name}>
-                <Card className="w-[100%]">
+                <Card>
                   <CardActionArea>
                     <CardMedia
                       component="img"
                       title={course.title}
                       image={course.image}
-                      height="200"
-                      width="300"
                     ></CardMedia>
                     <CardContent>
-                      <Typography>{course.name}</Typography>
+                      <Typography className="font-bold text-xl">
+                        {course.name}
+                      </Typography>
                     </CardContent>
                   </CardActionArea>
                   <CardActions>
--- utils/data.js
+++ utils/data.js
@@ -1,26 +1,26 @@
 const data = {
   courses: [
     {
-      name: "Websites",
+      name: "Web development course",
       image: "/website.jpeg",
       title: "Websites",
-      short_description: "Learn to build websites",
+      short_description: "View Curriculum",
       long_description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostr",
     },
     {
-      name: "Websites",
+      name: "Mobile App development course",
       image: "/mobile.jpg",
       title: "Websites",
-      short_description: "Learn to build websites",
+      short_description: "View Curriculum",
       long_description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostr",
     },
     {
-      name: "Websites",
+      name: "Desktop development course",
       image: "/desktop.png",
       title: "Websites",
-      short_description: "Learn to build websites",
+      short_description: "View Curriculum",
       long_description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostr",
     },
