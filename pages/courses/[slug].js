5d50da4835b55a3817044106296bbb909f3ba19a
Bralex-svg <alexadam269@gmail.com>
2023-03-22 16:38:01 -0700

    updated

---
+++ pages/courses/[slug].js
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router";
+import data from "../../utils/data";
+import Layout from "../../components/Layout";
+
+export default function ProductScreen() {
+  const router = useRouter();
+  const { slug } = router.query;
+  const course = data.courses.find((a) => a.slug === slug);
+  if (!course) {
+    return <div>Product not found</div>;
+  }
+  return (
+    <Layout title={course.name}>
+      <div>
+        <NextLink>
+          <Link>Back to products</Link>
+        </NextLink>
+      </div>
+    </Layout>
+  );
+}
